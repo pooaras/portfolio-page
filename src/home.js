2a96226899ede3df81a3b848f7df9ae3a5b9a1fa
pooaras <83764553+pooaras@users.noreply.github.com>
2023-02-23 14:04:54 +0530

    fodo

---
+++ src/home.js
@@ -0,0 +1,15 @@
+import './homestyle.css';
+
+export function Home(){
+
+    return( <section className="sec" >
+          <p >Pooarasu</p><br/>
+          <p >I'm passionate Web developer</p>
+          <div > </div> <br/>
+          <div >
+            <a href='https://www.linkedin.com/in/pooarasu-s/'>linkedin</a>
+            <a href='https://github.com/pooaras'>github</a>
+            <a href='mailto:pooarasusivaraj2002@gmail.com'>gmail</a>
+          </div>
+        </section>);
+}
